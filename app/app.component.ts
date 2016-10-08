dabf2cb26e4ce35b4089681ce7dae5b5f2ee91ed
pigeonT <pigeon.tian@gmail.com>
2016-10-08 21:12:46 +0200

    prototype

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'opened-tab',
+    template: `
+        <div>
+            
+        </div>
+    `
+})
+export class OpenedTab {
+
+
+}
---
+++ app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { OpenedTab }   from './app.component';
+
+
+@NgModule({
+  imports:      [ BrowserModule ],
+  declarations: [ OpenedTab ],
+})
+export class AppModule { }
